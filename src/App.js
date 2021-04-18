d9821847462b67863725fc51bb2153dc491a3f38
chilingirov <vasko988@gmail.com>
2021-04-17 17:25:28 -0700

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.id = null;
+    this.interval = 1000;
+    this.state = { time: 0, slow: 0, showButtons: true };
+  }
+
+  updateTimer = () => {
+    if (this.id) {
+      clearInterval(this.id);
+    }
+    this.id = setInterval(() => {
+      this.setState(
+        { time: this.state.time + 1, slow: this.state.slow + 1 },
+        () => {
+          this.increaseInterval();
+        }
+      );
+    }, this.interval);
+  };
+
+  increaseInterval = () => {
+    if (this.state.slow === 10) {
+      clearInterval(this.id);
+      this.interval += 300;
+      this.setState({ slow: 0 }, () => {
+        this.updateTimer();
+      });
+    }
+  };
+  startTimer = () => {
+    if (this.id) {
+      return;
+    }
+    this.updateTimer();
+  };
+  resetTimer = () => {
+    clearInterval(this.id);
+    this.setState({ time: 0, slow: 0 });
+    this.id = null;
+    this.interval = 1000;
+  };
+  showButtons = () => {
+    this.setState({ showButtons: true });
+  };
+  hideButtons = () => {
+    this.setState({ showButtons: false });
+  };
+  render() {
+    const { time, showButtons } = this.state;
+    return (
+      <div
+        className="App"
+        onMouseEnter={this.showButtons}
+        onMouseLeave={this.hideButtons}
+      >
+        <header className="App-header">
+          <p className="timer">{time}</p>
+          <div className={`buttons ${showButtons ? "" : "hidden"}`}>
+            <button className="button" onClick={this.startTimer}>
+              Start
+            </button>
+            <button className="button" onClick={this.resetTimer}>
+              Reset
+            </button>
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
